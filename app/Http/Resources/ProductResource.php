0a54d9ebc441fbe0b365432d906ef0648f61675c
Ferdy Hahan Pradana <75787853+ferdyhape@users.noreply.github.com>
2023-03-27 04:22:35 +0700

    add product and product detail api resource

---
+++ app/Http/Resources/ProductResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Http\Resources\CategoryResource;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        // return parent::toArray($request);
+        return [
+            'Nama' => $this->name,
+            'Kategori' => $this->category_id // display relationship data
+        ];
+    }
+}
